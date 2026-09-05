a70649b570d98959863d5c109abc8366c9afc630
Husam <husam.alarobe@gmail.com>
2026-09-05 22:01:26 +0300

    fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import { AUTH_STORAGE_KEYS } from './src/utiles/keys/auth.ts'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
 
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: AUTH_STORAGE_KEYS.Base_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        ws: true, // Enable WebSocket support
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss(),
+    ],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          ws: true,
+        },
       },
     },
-  },
+  }
 })
